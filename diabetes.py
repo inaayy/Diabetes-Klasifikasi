20e9cd8a71563325c397946b4e2cac7ebc579604
inaayy <119003121+inaayy@users.noreply.github.com>
2024-05-30 18:34:32 +0700

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -4,7 +4,7 @@ import numpy as np
 # import matplotlib.pyplot as plt
 # import seaborn as sns
 
-from imblearn.over_sampling import SMOTE # deal with imbalance data
+# from imblearn.over_sampling import SMOTE # deal with imbalance data
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
